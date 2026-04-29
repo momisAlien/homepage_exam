6e4eb3e0a01f49aaaab4e1b52b2004630159276f
User <user@example.com>
2026-04-29 17:02:07 +0900

    Fix Next Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { FormEvent, useEffect, useState } from "react";
+import type { FormEvent } from "react";
+import { useEffect, useState } from "react";
 import KakaoRoughMap from "@/components/KakaoRoughMap";
 
 const photos = [
